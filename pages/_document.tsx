6e2463d100a65eb402edec9aca17da2c3aa79a0c
Omar Rodríguez <astrek98@gmail.com>
2023-01-31 01:40:03 -0600

    feat: add form to get gift suggestions
    Add first version of the form to generate gift suggestions based on a description about the person.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <meta name="description" content="Gift thinker for any occasion" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/gift_32.png" />
+      </Head>
+      <body className="p-4 text-xl flex justify-center">
         <Main />
         <NextScript />
       </body>
