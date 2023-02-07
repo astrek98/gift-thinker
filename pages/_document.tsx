cadc3be765f8deb202f4ceedf6d57afaba6c3582
Omar Rodríguez <astrek98@gmail.com>
2023-02-06 19:53:36 -0600

    build: add mantine components library

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="description" content="Gift thinker for any occasion" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/gift-bag_64.png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/gift-bag_64.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
